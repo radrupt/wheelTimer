4065ca9c97dbb8e368ae7232818911688dff5d53
radrupt <962889483@qq.com>
2019-01-19 17:04:41 +0800

    update

---
+++ example/Activity.go
@@ -0,0 +1,32 @@
+package example
+
+// 签到定时提醒
+
+import (
+	"github.com/radrupt/wheelTimer"
+	"time"
+	"fmt"
+)
+type Activity struct {
+	W *wheelTimer.WheelTimer
+	remainingRounds int
+}
+func (task *Activity) Create(
+	w *wheelTimer.WheelTimer,
+) (*Activity, error) {
+	return &Activity{
+		W: w,
+	}, nil
+}
+
+func (t *Activity) Run(){
+	fmt.Println("excute activity", time.Now())
+}
+
+func (t *Activity) UpdateRemainingRounds(i int){
+	t.remainingRounds = i
+}
+
+func (t *Activity) GetRemainingRounds() int {
+	return t.remainingRounds
+}
